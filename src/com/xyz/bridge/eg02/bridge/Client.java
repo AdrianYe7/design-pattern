25d79e93ba18e1bcc0569f1403aefab51a477d93
Adrian <3144624477@qq.com>
2017-03-28 09:14:45 +0800

    编码修改

--- src/com/xyz/bridge/eg02/bridge/Client.java
+++ src/com/xyz/bridge/eg02/bridge/Client.java
@@ -4,7 +4,7 @@ public class Client {
     public static void main(String[] args) {
         MessageSender sender = new MobileMessageSender();
         AbstractMessage message = new UrgencyMessage(sender);
-        message.sendMessage("Çë¼Ù", "¾­Àí");
+        message.sendMessage("è¯·å‡", "ç»ç†");
         if(message instanceof UrgencyMessage) {
             UrgencyMessage m = (UrgencyMessage)message;
             m.watch("0001");
